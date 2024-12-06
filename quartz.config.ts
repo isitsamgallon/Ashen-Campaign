f03a0f37a543a057a4fb8dc3c3bf992eb939ec8c
Sam Gallon <samgallon@Sam-Gallons-Laptop-2.local>
2024-12-06 21:00:49 +0000

    Quartz sync: Dec 6, 2024, 9:00 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -40,7 +40,7 @@ const config: QuartzConfig = {
           textHighlight: "#fff23688",
         },
         darkMode: {
-          light: "#0000ff",
+          light: "#161618", // bg
           lightgray: "#393639",
           gray: "#646464",
           darkgray: "#d4d4d4",
